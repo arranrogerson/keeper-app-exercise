593c723cd17576adc24a7f5398c044239b55eb19
arranrogerson <87150239+arranrogerson@users.noreply.github.com>
2024-11-12 17:19:32 -0800

    v0

---
+++ keeper-app/src/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note () {
+	return (
+		<div className="note">
+			<h1>Laundry</h1>
+			<p>Separate the whites from the darks.</p>
+		</div>
+	)
+}
+
+export default Note;
