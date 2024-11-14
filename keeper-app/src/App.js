c41c5fa4cf219f206e0826cfa5d527bdd6416aa6
arranrogerson <87150239+arranrogerson@users.noreply.github.com>
2024-11-14 02:20:50 -0800

    v0.1
    added ability to import a list of notes

--- keeper-app/src/App.js
+++ keeper-app/src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import Header from "./Header";
 import Footer from  "./Footer";
 import Note from "./Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note />
+      {notes.map(item => 
+        <Note 
+          key={item.key}
+          title={item.title}
+          content={item.content}
+        />
+      )}
       <Footer />
     </div>
   );
--- keeper-app/src/Note.js
+++ keeper-app/src/Note.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Note () {
-	return (
-		<div className="note">
-			<h1>Laundry</h1>
-			<p>Separate the whites from the darks.</p>
+function Note(props) {
+    return (
+        <div className="note">
+			<h1>{props.title}</h1>
+			<p>{props.content}</p>
 		</div>
-	)
+    )
 }
 
 export default Note;
